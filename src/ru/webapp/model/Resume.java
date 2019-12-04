8634945cd2bc6125c06619aa4254a418aa496bd5
Igor Kapu <fff@mail.ru>
2019-12-04 16:25:36 +0300

    Added Resume class at the project

---
+++ src/ru/webapp/model/Resume.java
@@ -0,0 +1,17 @@
+package ru.webapp.model;
+
+import java.util.Collection;
+
+/**
+ * Капу пк
+ * 04.12.2019
+ */
+public class Resume {
+    private String fullName;
+    private String objective;
+    private String wantedObjective;
+    private Collection<WorkPlace> expierence;
+    private Collection<String> achievement;
+    private String qualification;
+    private String education;
+}
