9ca6110550e34fb29375ba723165d626ffa19eaf
Igor Kapu <fff@mail.ru>
2019-12-04 16:26:20 +0300

    Added Resume class at the project

---
+++ src/ru/webapp/model/WorkPlace.java
@@ -0,0 +1,14 @@
+package ru.webapp.model;
+
+import java.util.Date;
+
+/**
+ * Капу пк
+ * 04.12.2019
+ */
+class WorkPlace {
+    private String companyName;
+    private int salary;
+    private Date firstWorksMonth;
+    private Date lastWorksMonth;
+}
