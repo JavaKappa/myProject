81188d892bca4c31e132ba0129a25bab3911a430
Igor Kapu <fff@mail.ru>
2019-12-06 00:21:59 +0300

    Modified ContactType.java

--- src/ru/webapp/model/ContactType.java
+++ src/ru/webapp/model/ContactType.java
@@ -1,8 +1,18 @@
 package ru.webapp.model;
 
 public enum ContactType {
-    PHONE_NUMBER,
-    EMAIL,
-    SKYPE,
-    HOME_ADDRESS,
+    PHONE_NUMBER("Номер телефона"),
+    EMAIL ("Электронная почта"),
+    SKYPE("Skype"),
+    HOME_ADDRESS("Домашний адрес");
+
+    private String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
