81006b78766e0df514647ea701a4cc6c595a04de
Igor Kapu <fff@mail.ru>
2019-12-06 00:17:45 +0300

    Updated SectionType added constructor and values

--- src/ru/webapp/model/SectionType.java
+++ src/ru/webapp/model/SectionType.java
@@ -1,9 +1,16 @@
 package ru.webapp.model;
 
 public enum SectionType {
-    OBJECTIVE,
-    ACHIEVEMENT,
-    QUALIFICATIONS,
-    EXPERIENCE,
-    EDUCATION
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт"),
+    EDUCATION("Образование");
+
+    private String title;
+
+
+    SectionType(String title) {
+        this.title = title;
+    }
 }
