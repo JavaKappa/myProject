648d3f62145829f34a0904aec8021c98044123b0
JavaKappa <nextnation7@gmail.com>
2019-12-25 21:00:05 +0300

    Test servlet

--- src/ru/webapp/web/ResumeServlet.java
+++ src/ru/webapp/web/ResumeServlet.java
@@ -1,7 +1,12 @@
 package ru.webapp.web;
 
+import ru.webapp.storage.WebAppException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 
 public class ResumeServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
@@ -9,7 +14,14 @@ public class ResumeServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-
+        try (Writer writer = response.getWriter();){
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html; charset=UTF-8");
+            String name = request.getParameter("name");
+            writer.write("Тест сервлет: " + name);
+        } catch (IOException e) {
+            throw new WebAppException("get writer failed", e);
+        }
     }
 
 }
